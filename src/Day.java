3982316af5636f75713688759564ea30c275e1b0
ViktorLukashchuk <lukashchuk.victor@gmail.com>
2021-01-31 23:22:14 +0200

    modification due to additional request

--- src/Day.java
+++ src/Day.java
@@ -1,37 +1,41 @@
 import java.util.Scanner;
 
 enum WeekDays {
+		
 	MONDAY {
 		@Override
-        public String nextDay() { return "TUESDAY"; }
+		public String nextDay() { return WeekDays.MONDAY.next().name();}
     	},
     TUESDAY {
     	@Override
-        public String nextDay() { return "WEDNESDAY"; }
+        public String nextDay() { return WeekDays.TUESDAY.next().name(); }
     	},
 	WEDNESDAY {
     	@Override
-    	public String nextDay() { return "THURSDAY"; }
+    	public String nextDay() { return WeekDays.WEDNESDAY.next().name(); }
 		},
 	THURSDAY {
 		@Override
-		public String nextDay() { return "FRIDAY"; }
+		public String nextDay() { return WeekDays.THURSDAY.next().name(); }
 		},
 	FRIDAY {
 		@Override
-		public String nextDay() { return "SATURDAY"; }
+		public String nextDay() { return WeekDays.FRIDAY.next().name(); }
 		},
 	SATURDAY {
 		@Override
-		public String nextDay() { return "SUNDAY"; }
+		public String nextDay() { return WeekDays.SATURDAY.next().name(); }
 		},
 	SUNDAY {
 		@Override
-	    public String nextDay() { return "MONDAY"; }
+	    public String nextDay() { return WeekDays.SUNDAY.next().name(); }
 		 };
 
-    public abstract String nextDay();
-
+    abstract String nextDay();
+    static final WeekDays[] values = values();
+    public WeekDays next() {
+        return values[(ordinal() + 1) % values.length];
+    }
 }
 
 public class Day {
@@ -44,25 +48,28 @@ public class Day {
 		
 		switch(day.toLowerCase()) {
 		case "monday":
-			System.out.println("The next day after " + day + " is " + WeekDays.MONDAY.nextDay());
+			System.out.println("The next day after Monday is " + WeekDays.MONDAY.nextDay());
 			break;
 		case "tuesday":
-			System.out.println("The next day after " + day + " is " + WeekDays.TUESDAY.nextDay());
+			System.out.println("The next day after Tuesday is " + WeekDays.TUESDAY.nextDay());
 			break;
 		case "wednesday":
-			System.out.println("The next day after " + day + " is " + WeekDays.WEDNESDAY.nextDay());
+			System.out.println("The next day after Wednesday is " + WeekDays.WEDNESDAY.nextDay());
 			break;
 		case "thursday":
-			System.out.println("The next day after " + day + " is " + WeekDays.THURSDAY.nextDay());
+			System.out.println("The next day after Thursday is " + WeekDays.THURSDAY.nextDay());
 			break;
 		case "friday":
-			System.out.println("The next day after " + day + " is " + WeekDays.FRIDAY.nextDay());
+			System.out.println("The next day after Friday is " + WeekDays.FRIDAY.nextDay());
 			break;
 		case "saturday":
-			System.out.println("The next day after " + day + " is " + WeekDays.SATURDAY.nextDay());
+			System.out.println("The next day after Saturday is " + WeekDays.SATURDAY.nextDay());
 			break;
 		case "sunday":
-			System.out.println("The next day after " + day + " is " + WeekDays.SUNDAY.nextDay());
+			System.out.println("The next day after Sunday is " + WeekDays.SUNDAY.nextDay());
+			break;
+		default:
+			System.out.println("UNKNOWN input. Please run the program again.");
 			break;
 		}
 				

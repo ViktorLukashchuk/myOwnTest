385ec7bf06154cac83a7a80b491ca41b235a1324
ViktorLukashchuk <lukashchuk.victor@gmail.com>
2021-03-01 23:13:02 +0200

    jackson homework

---
+++ MyTest/src/main/java/pages/Position.java
@@ -0,0 +1,73 @@
+package pages;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class Position {
+    private String position;
+    private String positionText;
+    private String points;
+    private String wins;
+
+    @JsonProperty("Driver")
+    private Driver driver;
+
+    @JsonProperty("Constructors")
+    private List<Constructors> constructors;
+
+
+    public List<Constructors> getConstructors() {
+        return constructors;
+    }
+
+    public void setConstructors(List<Constructors> constructors) {
+        this.constructors = constructors;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPositionText() {
+        return positionText;
+    }
+
+    public void setPositionText(String positionText) {
+        this.positionText = positionText;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+
+    public String getWins() {
+        return wins;
+    }
+
+    public void setWins(String wins) {
+        this.wins = wins;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    public String toString(){
+        return "    \nPosition: " + position + ",    \n positionText: " + positionText + ",    \n points: " + points + ",    \n wins: " + wins
+                + ",\n    " + driver + ",\n    " + constructors ;
+    }
+}
